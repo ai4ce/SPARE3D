7f68db7528101528d98dd71530521ed6fdcbdbf4
WenyuHan-LiNa <55855538+WenyuHan-LiNa@users.noreply.github.com>
2020-03-29 17:54:52 -0400

    Update Dataloader.py

--- Code/Isometric_to_pose/Dataloader.py
+++ Code/Isometric_to_pose/Dataloader.py
@@ -13,25 +13,26 @@ import cv2
 class I2P_data(Dataset):
     def __init__(self, root_dir, transform=None):
         self.dic=sorted(os.listdir(root_dir))
+        self.dic.remove('answer.json')
         self.transform=transform
         self.root_dir=root_dir
+        with open(os.path.join(self.root_dir, 'answer.json'), 'r') as f:
+            self.answer = json.load(f)
     def __getitem__(self, idx):   
         if torch.is_tensor(idx):
             idx = idx.tolist()        
         input_dic = os.path.join(self.root_dir,
-                                self.dic[idx],"Dic3V")
+                                self.dic[idx])
        
         
-        output_dic = os.path.join(self.root_dir,
-                                self.dic[idx],"DicAns")
-  
+        answer = self.answer[self.dic[idx]]
     
-        Front_img=self.convert_input(input_dic,"/*front.png")
-        Right_img=self.convert_input(input_dic,"/*right.png")
-        Top_img=self.convert_input(input_dic,"/*top.png")
+        Front_img=self.convert_input(input_dic,"/*f.png")
+        Right_img=self.convert_input(input_dic,"/*r.png")
+        Top_img=self.convert_input(input_dic,"/*t.png")
        
         Ans=self.convert_input(input_dic,"/answer.png")
-        Label=self.convert_answer(output_dic)
+        Label=self.convert_answer(answer)
         View=np.concatenate((Front_img,Right_img,Top_img),axis=2)
         group=np.moveaxis(np.concatenate((Ans,View),axis=2),-1,0)
         return group,Label
@@ -43,16 +44,14 @@ class I2P_data(Dataset):
         if self.transform:
             img=self.transform(img)
         return img/255
-    def convert_answer(self,Dic):
-        index=(sorted(os.listdir(Dic)))[0]
-        index=index.replace(".txt","")
-
-        if index=="topleft":
+    def convert_answer(self,index):
+        
+        if index=="0":
             output=torch.tensor([0])
-        if index=="bottomleft":
+        if index=="1":
             output=torch.tensor([1])
-        if index=="topright":
+        if index=="2":
             output=torch.tensor([2])
-        if index=="bottomright":
+        if index=="3":
             output=torch.tensor([3])
         return output
