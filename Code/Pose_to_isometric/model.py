9e8a3be812ec4d865bb872318c9eaad54162342e
WenyuHan-LiNa <55855538+WenyuHan-LiNa@users.noreply.github.com>
2020-04-14 18:29:29 -0400

    Update model.py

--- Code/Pose_to_isometric/model.py
+++ Code/Pose_to_isometric/model.py
@@ -4,57 +4,11 @@ from torchvision import datasets, models, transforms
 import torch.nn as nn
 import torch
 import glob
-from PIL import Image
-
 import os
 import torch
-from skimage import io, transform
 import numpy as np
-import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
-import cv2
 import time
-from matplotlib import pyplot as plt  
 import bagnets.pytorchnet
-
-
-class Rescale(object):
-    """Rescale the image in a sample to a given size.
-
-    Args:
-        output_size (tuple or int): Desired output size. If tuple, output is
-            matched to output_size. If int, smaller of image edges is matched
-            to output_size keeping aspect ratio the same.
-    """
-
-    def __init__(self, output_size):
-        assert isinstance(output_size, (int, tuple))
-        self.output_size = output_size
-
-    def __call__(self, sample):
-        image = sample
-
-        h, w = image.shape[:2]
-        if isinstance(self.output_size, int):
-            if h > w:
-                new_h, new_w = self.output_size * h / w, self.output_size
-            else:
-                new_h, new_w = self.output_size, self.output_size * w / h
-        else:
-            new_h, new_w = self.output_size
-
-        new_h, new_w = int(new_h), int(new_w)
-        dim=(new_h, new_w)
-
-        Img= cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-
-        # h and w are swapped for landmarks because for images,
-        # x and y axes are axis 1 and 0 respectively
-      
-
-        return Img
-
 
 
 
